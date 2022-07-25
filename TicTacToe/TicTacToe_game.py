c40d00309b7889fd3e34ffe6ab831891cd22e725
nnnLik <makhmudov_ra@vk.com>
2022-07-25 10:35:17 +0300

    feat(ttt.game):processing the player turn

--- TicTacToe/TicTacToe_game.py
+++ TicTacToe/TicTacToe_game.py
@@ -1,10 +1,19 @@
 import tkinter
 
-
 class TicTacToe(tkinter.Canvas):
-
     def __init__(self, window):
         super().__init__(window, width=300, height=300)
+        self.state = [None, ] * 9
+        self.bind('<Button-1>', self.click)
+
+    def click(self, event):
+        column = int(event.x / 100)
+        row = int(event.y / 100)
+        index = row * 3 + column
+        if self.state[index] is None:
+            self.state[index] = 'x'
+            self.add_x(column, row)
+
 
     def add_x(self, column, row):
         self.create_line(
@@ -33,7 +42,6 @@ class TicTacToe(tkinter.Canvas):
             width=5,
             outline='dark salmon'
         )
-
 
 tk_window = tkinter.Tk()
 game = TicTacToe(tk_window)
