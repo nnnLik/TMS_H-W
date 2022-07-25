66e0660f2e3047cb8e096b24344845d39c6446ae
nnnLik <makhmudov_ra@vk.com>
2022-07-25 10:29:03 +0300

    feat(ttt.game):we can draw

--- TicTacToe/TicTacToe_game.py
+++ TicTacToe/TicTacToe_game.py
@@ -1,8 +1,38 @@
 import tkinter
 
+
 class TicTacToe(tkinter.Canvas):
+
     def __init__(self, window):
         super().__init__(window, width=300, height=300)
+
+    def add_x(self, column, row):
+        self.create_line(
+            column * 100 + 20,
+            row * 100 + 20,
+            column * 100 + 80,
+            row * 100 + 80,
+            width=5,
+            fill='dark blue'
+        )
+        self.create_line(
+            column * 100 + 80,
+            row * 100 + 20,
+            column * 100 + 20,
+            row * 100 + 80,
+            width=5,
+            fill='dark blue'
+        )
+
+    def add_o(self, column, row):
+        self.create_oval(
+            column * 100 + 20,
+            row * 100 + 20,
+            column * 100 + 80,
+            row * 100 + 80,
+            width=5,
+            outline='dark salmon'
+        )
 
 
 tk_window = tkinter.Tk()
