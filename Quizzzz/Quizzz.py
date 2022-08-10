1c66f51a50091613637d013de427eb08b71c286e
nnnLik <makhmudov_ra@vk.com>
2022-08-10 17:37:53 +0300

    fix question set

--- Quizzzz/Quizzz.py
+++ Quizzzz/Quizzz.py
@@ -8,48 +8,22 @@ class Quizzz:
         self.question=que
         self.correct_answer=crt_answ
         self.answers=answ
-        shuffle(self.answers)
-        self.list_out()
 
-    def list_out(self):
-        for i in range(len(self.answers)):
-            self.answers.insert(i, f"{i + 1}. {self.answers[i]}")
-            self.answers.pop(i + 1)
+
 
     @property
     def start(self):
-        if self.answers[int(input("Enter the answer number - ")) - 1][3:] == self.correct_answer:
+
+        if self.answers[0][int(input("Enter the answer number - ")) - 1] == self.correct_answer:
             Quizzz.score += 1
             return True
         else:
             return False
 
-    def __str__(self):
-        return "\n" + self.question + "\n" + "\n".join(self.answers) + "\n" + f"Your points --> {Quizzz.score}"
+example1 = Quizzz(que='What is the capital of Poland?', crt_answ="Warsaw",
+              answ=['Minsk', 'Warsaw', 'Brest'])
+example2 = Quizzz(que='What is the capital of USA?', crt_answ="Washington",
+              answ=['Washington', 'Kazakhstan', 'Kentucky'])
+example3 = Quizzz(que='What is the capital of Madagascar?', crt_answ="Antananarivo",
+              answ=['Wakanda', 'Disney','Antananarivo'])
 
-Que = [
-
-    'The capital of Poland',
-    'The capital of USA',
-    'The capital of Madagascar'
-]
-
-Correct_answs = [
-
-    'Warsaw',
-    'Washington',
-    'Antananarivo'
-
-]
-
-Answers = [
-
-    ['Minsk', 'Warshawa', 'Brest'],
-    ['Washington', 'Kazakhstan', 'Kentucky'],
-    ['Wakanda', 'Disney','Antananarivo']
-]
-
-
-example1=Quizzz(Que, Correct_answs, Answers)
-
-print(example1.start)
